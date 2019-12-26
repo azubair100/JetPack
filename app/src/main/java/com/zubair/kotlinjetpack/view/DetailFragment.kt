c7a6283a7a94a245e572371ac5a9a10b44264b68
Ahmed Zubair <iambatman8102@gmail.com>
2019-12-25 23:54:01 -0500

    navigatoin basics

---
+++ app/src/main/java/com/zubair/kotlinjetpack/view/DetailFragment.kt
@@ -0,0 +1,21 @@
+package com.zubair.kotlinjetpack.view
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.zubair.kotlinjetpack.R
+
+class DetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.detail_fragment, container, false)
+
+
+
+}
