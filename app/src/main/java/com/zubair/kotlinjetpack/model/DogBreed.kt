5010c0bb00fa3a3829c36eaa323d5731023e26e3
Ahmed Zubair <iambatman8102@gmail.com>
2019-12-26 03:40:58 -0500

    master merged

---
+++ app/src/main/java/com/zubair/kotlinjetpack/model/DogBreed.kt
@@ -0,0 +1,12 @@
+package com.zubair.kotlinjetpack.model
+
+data class DogBreed(
+    val id : String?,
+    val name: String?,
+    val lifespan: String?,
+    val group: String?,
+    val purpose: String?,
+    val temperament: String?,
+    val url: String
+
+)
